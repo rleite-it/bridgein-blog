d7a3bed2f80ff7086f50d78d97194be631d1dbbf
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-13 19:40:17 +0100

    Update app file (add header and main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import { Button } from "@/components/ui/button";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "@/routes";
+import Navbar from "./components/navbar";
+
+import "./App.css";
 
 function App() {
 	return (
 		<>
-			<Button>Click me</Button>
-			<Routes>
-				{ROUTES.map((route) => (
-					<Route
-						key={route.id}
-						path={route.path}
-						element={route.element}
-						errorElement={route?.errorElement}
-					/>
-				))}
-			</Routes>
+			<header className="w-[100vw] h-[4rem] sticky top-0 flex justify-center items-baseline">
+				<Navbar />
+			</header>
+			<main className="mt-5">
+				<Routes>
+					{ROUTES.map((route) => (
+						<Route
+							key={route.id}
+							path={route.path}
+							element={route.element}
+							errorElement={route?.errorElement}
+						/>
+					))}
+				</Routes>
+			</main>
 		</>
 	);
 }
