558744d62f1b9afb99bfce050cd73b4a35585002
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-13 18:54:50 +0100

    Add theme-provider component to our application root

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
 	</React.StrictMode>
 );
