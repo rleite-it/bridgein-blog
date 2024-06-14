e05f4e8d1484bfb4dc5774b05640b05f8e93c925
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-14 17:09:43 +0100

    Add delete and add comment to the posts

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,11 +1,20 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { PostProps } from "@/types/Post";
+import { useUser } from "./useUser";
+import { UserProps } from "@/types/User";
+import { CommentProps } from "@/types/Comment";
 
 export const usePosts = () => {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [page, setPage] = useState(0);
-	const [hasMore, setHasMore] = useState(true);
+	const [data, setData] = useState<PostProps[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [page, setPage] = useState<number>(0);
+	const [hasMore, setHasMore] = useState<boolean>(true);
+	const [postId, setPostId] = useState<number | null>(null);
+	const [selectedPost, setSelectedPost] = useState<PostProps | null>(null);
+	const [openPost, setOpenPost] = useState<boolean>(false);
+	const [user, setUser] = useState<UserProps | null>(null);
+	const { loadingUser, fetchUser } = useUser();
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -32,5 +41,91 @@ export const usePosts = () => {
 		fetchPosts();
 	}, [page]);
 
-	return { data, loading, hasMore, setPage };
+	useEffect(() => {
+		if (openPost) {
+			const fecthPostById = async () => {
+				try {
+					const { data } = await axios.get(
+						`${
+							import.meta.env.VITE_BASE_API_URL
+						}/posts/${postId}?_embed=comments`
+					);
+
+					const userData = await fetchUser(data.userId);
+
+					if (!loadingUser) {
+						setUser(userData);
+						setSelectedPost(data);
+					}
+				} catch (error) {
+					console.error("Error fetching post:", error);
+				}
+			};
+
+			if (!selectedPost || selectedPost.id !== postId) fecthPostById();
+		}
+	}, [openPost]);
+
+	const addComment = (value: string) => {
+		if (selectedPost !== null) {
+			const commentLayout: CommentProps = {
+				postId: selectedPost?.id,
+				id: selectedPost?.comments && selectedPost?.comments?.length + 1,
+				email: "user@bridgein.com",
+				name: "User",
+				body: value,
+			};
+
+			const updatedPost = {
+				...selectedPost,
+				comments: [commentLayout, ...selectedPost.comments],
+			};
+
+			const result = data.map((post) =>
+				post.id === selectedPost.id ? updatedPost : post
+			);
+
+			setSelectedPost(updatedPost); // Update with a new object
+			setData(result);
+		}
+	};
+
+	const deleteComment = (commentId: number) => {
+		if (selectedPost !== null) {
+			const commentIndex = selectedPost.comments?.findIndex(
+				(comment) => comment.id === commentId
+			);
+
+			const commentsArr = selectedPost.comments;
+			commentsArr?.splice(commentIndex, 1);
+
+			const updatedPost = {
+				...selectedPost,
+				comments: commentsArr,
+			};
+
+			const result = data.map((post) =>
+				post.id === selectedPost.id ? updatedPost : post
+			);
+
+			setSelectedPost(updatedPost); // Update with a new object
+			setData(result);
+		}
+	};
+
+	return {
+		data,
+		loading,
+		hasMore,
+		setPage,
+		openPost,
+		setOpenPost,
+		selectedPost,
+		setSelectedPost,
+		postId,
+		setPostId,
+		user,
+		addComment,
+		deleteComment,
+	};
 };
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,10 +1,34 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Post from "@/components/post";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import Comment from "@/components/comment";
 import { usePosts } from "@/hooks/usePosts";
+import { useUser } from "@/hooks/useUser";
+
 import { LoadingIcon } from "@/assets/icons/loading";
 
 function Feed() {
-	const { data, loading, hasMore, setPage } = usePosts();
+	const {
+		data,
+		loading,
+		hasMore,
+		setPage,
+		openPost,
+		setOpenPost,
+		selectedPost,
+		setPostId,
+		user,
+		addComment,
+		deleteComment,
+	} = usePosts();
+	const { loadingUser } = useUser();
+	const [inputValue, setInputValue] = useState<string | null>(null);
 
 	const handleScroll = useCallback(() => {
 		if (
@@ -31,9 +55,12 @@ function Feed() {
 				{data.map((post) => (
 					<Post
 						key={`post-${post.id}`}
+						id={post.id}
 						title={post.title}
 						body={post.body}
-						numComments={post.comments.length}
+						numComments={post.comments ? post.comments.length : 0}
+						openPost={setOpenPost}
+						postId={setPostId}
 					/>
 				))}
 
@@ -43,6 +70,85 @@ function Feed() {
 					</div>
 				)}
 			</div>
+			<Dialog open={openPost} onOpenChange={setOpenPost}>
+				<DialogContent
+					className="max-w-[800px] min-w-[700px]"
+					onInteractOutside={(e) => e.preventDefault()}
+				>
+					{selectedPost !== null && !loadingUser ? (
+						<>
+							<DialogHeader>
+								<DialogTitle>{selectedPost.title}</DialogTitle>
+							</DialogHeader>
+							<div className="flex flex-col gap-4 pt-4 border-t-2">
+								<div className="flex gap-4 w-full justify-between min-h-fit">
+									<p className="max-w-[70%]">{selectedPost.body}</p>
+									<hr className="w-0 min-h-[100%] border-[1px]" />
+									<div className="min-w-fit flex flex-col flex-1">
+										<p className="font-bold">
+											Author:{" "}
+											<span className="font-normal">{user?.name ?? ""}</span>
+										</p>
+										<p className="font-bold">
+											Email:{" "}
+											<span className="font-normal">{user?.email ?? ""}</span>
+										</p>
+										<p className="font-bold">
+											Website:{" "}
+											<a
+												href={`https://${user?.website}`}
+												target="_blank"
+												rel="noopener noreferrer"
+												className="font-normal"
+											>
+												{user?.website ?? ""}
+											</a>
+										</p>
+									</div>
+								</div>
+								<div className="pt-8 flex flex-col gap-6">
+									<Textarea
+										placeholder="Write a comment..."
+										value={inputValue ?? ""}
+										onChange={(e) => setInputValue(e.target.value)}
+										onKeyDown={(e) => {
+											if (e.code === "Enter" && inputValue) {
+												if (inputValue.trim().length) addComment(inputValue);
+												setInputValue("");
+											}
+										}}
+										onKeyUp={(e) => {
+											if (e.code === "Enter") setInputValue("");
+										}}
+									/>
+									<div className="max-h-[500px] overflow-auto w-full border-[1px] p-4 rounded-md flex flex-col gap-4">
+										{selectedPost.comments?.length ? (
+											selectedPost.comments.map((comment) => (
+												<Comment
+													key={`comment-${comment.id}`}
+													author={comment.email}
+													body={comment.body}
+													editComment={() => console.log("editou")}
+													deleteComment={() => deleteComment(comment.id)}
+													isAuthor={comment.email === user?.email}
+												/>
+											))
+										) : (
+											<span className="font-thin italic text-center">
+												Não tem comentários!
+											</span>
+										)}
+									</div>
+								</div>
+							</div>
+						</>
+					) : (
+						<div className="w-full flex justify-center items-center">
+							<LoadingIcon width="3rem" height="3rem" />
+						</div>
+					)}
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 }
