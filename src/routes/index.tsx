82727a8d48109489fc596bce3eb26d1317f34418
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-14 15:02:31 +0100

    Remove unused route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import { RouteProps, Navigate } from "react-router-dom";
 
 const Feed = React.lazy(() => import("@/pages/Feed"));
-const Users = React.lazy(() => import("@/pages/Users"));
 
 export const ROUTES: RouteProps[] = [
 	{
 		id: "feed",
 		path: "/feed",
 		element: <Feed />,
-	},
-	{
-		id: "users",
-		path: "/users",
-		element: <Users />,
 	},
 	{
 		id: "index",
