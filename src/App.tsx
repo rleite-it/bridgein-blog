60c47a4c5739655287add8c7028bef8533184b44
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-14 13:40:55 +0100

    Update App file

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,17 @@ import { ROUTES } from "@/routes";
 import Loading from "./pages/Loading";
 import Navbar from "./components/navbar";
 
-import "./App.css";
-
 function App() {
 	return (
 		<>
-			<header className="w-[100vw] h-[4rem] bg-white dark:bg-[#0A0A0A] sticky top-0 flex justify-center items-baseline">
+			<header className="w-[100%] h-[4rem] bg-white dark:bg-[#0A0A0A] sticky top-0 flex justify-center items-baseline">
 				<Navbar />
 			</header>
 			<React.Suspense fallback={<Loading />}>
 				<main className="mt-10 mx-auto w-full h-full flex justify-center">
 					<Routes>
 						{ROUTES.map((route) => (
-							<Route
-								key={route.id}
-								path={route.path}
-								element={route.element}
-								errorElement={route?.errorElement}
-							/>
+							<Route key={route.id} path={route.path} element={route.element} />
 						))}
 					</Routes>
 				</main>
