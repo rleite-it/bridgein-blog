9a5fcc5612a90ec4b0c4de4fbfe8a9e7138a2a81
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-14 15:03:12 +0100

    Update post component

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,19 +1,33 @@
+import { PostProps } from "@/types/Post";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 import ChatBubbleIcon from "@/assets/icons/chat-bubble.tsx";
 
-type PostProps = {
-	title: string;
-	body: string;
+type PostItemProps = {
 	numComments: number;
-};
+	openPost: (val: boolean) => void;
+	postId: (id: number) => void;
+} & PostProps;
 
-function Post({ title, body, numComments }: PostProps) {
+function Post({
+	id,
+	title,
+	body,
+	numComments,
+	openPost,
+	postId,
+}: PostItemProps) {
 	return (
 		<article>
 			<Card className="text-left">
 				<CardHeader className="w-full flex flex-row items-center justify-between  select-none">
-					<CardTitle className="w-fit transition-all duration-200 hover:text-[#cc0000] cursor-pointer">
+					<CardTitle
+						className="w-fit transition-all duration-200 hover:text-[#cc0000] cursor-pointer"
+						onClick={() => {
+							openPost(true);
+							postId(id);
+						}}
+					>
 						{title}
 					</CardTitle>
 					<div className="flex gap-2 items-center transition-all duration-200 hover:text-[#cc0000]">
