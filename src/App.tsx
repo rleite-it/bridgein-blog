022a003ef16fdf685a5926edadc1844eed3d5c57
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-06-14 12:27:30 +0100

    Add lazy import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "@/routes";
+import Loading from "./pages/Loading";
 import Navbar from "./components/navbar";
 
 import "./App.css";
@@ -7,21 +9,23 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<header className="w-[100vw] h-[4rem] sticky top-0 flex justify-center items-baseline">
+			<header className="w-[100vw] h-[4rem] bg-white dark:bg-[#0A0A0A] sticky top-0 flex justify-center items-baseline">
 				<Navbar />
 			</header>
-			<main className="mt-5">
-				<Routes>
-					{ROUTES.map((route) => (
-						<Route
-							key={route.id}
-							path={route.path}
-							element={route.element}
-							errorElement={route?.errorElement}
-						/>
-					))}
-				</Routes>
-			</main>
+			<React.Suspense fallback={<Loading />}>
+				<main className="mt-10 mx-auto w-full h-full flex justify-center">
+					<Routes>
+						{ROUTES.map((route) => (
+							<Route
+								key={route.id}
+								path={route.path}
+								element={route.element}
+								errorElement={route?.errorElement}
+							/>
+						))}
+					</Routes>
+				</main>
+			</React.Suspense>
 		</>
 	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
+import React from "react";
 import { RouteProps } from "react-router-dom";
+
+const Feed = React.lazy(() => import("@/pages/Feed"));
+const Users = React.lazy(() => import("@/pages/Users"));
 
 export const ROUTES: RouteProps[] = [
 	{
 		id: "feed",
 		path: "/feed",
-		element: <h1>FEED</h1>,
+		element: <Feed />,
 	},
 	{
 		id: "users",
 		path: "/users",
-		element: <h1>USERS</h1>,
+		element: <Users />,
 	},
 ];
